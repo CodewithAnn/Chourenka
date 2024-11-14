25d00f6bafcc350aae174e730cca020b057970f6
CodeWithAnn <codewithann@gmail.com>
2024-11-14 18:31:48 +0530

    crated model and tempory data

---
+++ lib/data/words.dart
@@ -0,0 +1,97 @@
+import 'package:chinese_flashcard/model/word.dart';
+
+final List<Word> words = [
+  Word(
+      topic: "Beach",
+      english: "Swimsuit",
+      character: "泳装",
+      pinyin: "yǒng zhuāng"),
+  Word(topic: "Beach", english: "Sand", character: "沙", pinyin: "shā"),
+  Word(
+      topic: "Beach",
+      english: "Seashell",
+      character: "海贝壳",
+      pinyin: "hǎi bèi ké"),
+  Word(topic: "Birds", english: "Parrot", character: "鹦鹉", pinyin: "yīng wǔ"),
+  Word(topic: "Birds", english: "Seabird", character: "海鸟", pinyin: "hǎi niǎo"),
+  Word(
+      topic: "Birds", english: "Owl", character: "猫头鹰", pinyin: "māo tóu yīng"),
+  Word(
+      topic: "Land Animals",
+      english: "Tiger",
+      pinyin: "Lǎo Hǔ",
+      character: "老虎"),
+  Word(topic: "Land Animals", english: "Bear", pinyin: "Xióng", character: "熊"),
+  Word(
+      topic: "Land Animals",
+      english: "Beaver",
+      pinyin: "Hǎi Lí",
+      character: "海狸"),
+  Word(
+      topic: "Land Animals",
+      english: "Camel",
+      pinyin: "Luò Tuó",
+      character: "骆驼"),
+  Word(topic: "Land Animals", english: "Dog", pinyin: "Gǒu", character: "狗"),
+  Word(
+      topic: "Marine Life",
+      english: "Dolphin",
+      pinyin: "Hǎi Tún",
+      character: "海豚"),
+  Word(
+      topic: "Marine Life",
+      english: "Shark",
+      pinyin: "Shā Yú",
+      character: "鲨鱼"),
+  Word(
+      topic: "Marine Life",
+      english: "Whale",
+      pinyin: "Jīng Yú",
+      character: "鲸鱼"),
+  Word(topic: "Music", english: "Piano", pinyin: "Gāng Qín", character: "钢琴"),
+  Word(topic: "Music", english: "Flute", pinyin: "Cháng Dí", character: "长笛"),
+  Word(topic: "Nature", english: "Tree", pinyin: "Shù", character: "树"),
+  Word(topic: "Nature", english: "Rock", pinyin: "Shí Tou", character: "石头"),
+  Word(
+      topic: "School",
+      english: "School Bag",
+      pinyin: "Shū Bāo",
+      character: "书包"),
+  Word(
+      topic: "School", english: "Computer", pinyin: "Diànnǎo", character: "电脑"),
+  Word(topic: "School", english: "Pen", pinyin: "Bǐ", character: "笔"),
+  Word(
+      topic: "Shapes",
+      english: "Triangle",
+      pinyin: "Sān Jiǎo Xíng",
+      character: "三角形"),
+  Word(
+      topic: "Shapes", english: "Circle", pinyin: "Yuán Xíng", character: "圆形"),
+  Word(
+      topic: "Shapes",
+      english: "Square",
+      pinyin: "Zhèng Fāng Xíng",
+      character: "正方形"),
+  Word(topic: "Sports", english: "Tennis", pinyin: "Wǎng Qiú", character: "网球"),
+  Word(
+      topic: "Sports",
+      english: "Rugby",
+      pinyin: "Gǎn Lǎn Qiú",
+      character: "橄榄球"),
+  Word(topic: "Sports", english: "Golf", pinyin: "Gāo'ěr Fū", character: "高尔夫"),
+  Word(
+      topic: "Vehicles",
+      english: "Motorbike",
+      pinyin: "Mó Tuō Chē",
+      character: "摩托车"),
+  Word(topic: "Vehicles", english: "Train", pinyin: "Huǒ Chē", character: "火车"),
+  Word(topic: "Vehicles", english: "Truck", pinyin: "Kǎ Chē", character: "卡车"),
+  Word(
+      topic: "Vehicles",
+      english: "Airplane",
+      pinyin: "Fēi Jī",
+      character: "飞机"),
+  Word(topic: "Weather", english: "Rain", pinyin: "Xià Yǔ", character: "下雨"),
+  Word(topic: "Weather", english: "Sun", pinyin: "Tài Yáng", character: "太阳"),
+  Word(topic: "Weather", english: "Cloud", pinyin: "Yún", character: "云"),
+];
---
+++ lib/model/word.dart
@@ -0,0 +1,16 @@
+class Word {
+  /// [topic] topic/theme of the word
+  final String topic; 
+  /// [english] word 
+  final String english;
+  /// [character] in the language here is chinese
+  final String character;
+  /// [pinyin] Roman word system to pronounce a word in chinese
+  final String pinyin;
+
+  Word(
+      {required this.topic, 
+      required this.english,
+      required this.character,
+      required this.pinyin});
+}
