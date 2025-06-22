193498066ecf7b9e4932e242fdce410a47f58d24
CodewithAnn <de.anand.dev@gmail.com>
2025-06-22 23:38:51 +0530

    Migration to fvm 3.32.0
    í¿¢ Fixed compartibilty  with gradle and kotline versions
    í¿¢ Remove ProviderScope() for migration to Bloc
    í¿¢ Added FVM support for repository

--- lib/model/word.dart
+++ lib/model/word.dart
@@ -1,16 +1,15 @@
 class Word {
   /// [topic] topic/theme of the word
-  final String topic; 
-  /// [english] word 
+  final String topic;
+
+  /// [english] word
   final String english;
+
   /// [character] in the language here is chinese
   final String character;
+
   /// [pinyin] Roman word system to pronounce a word in chinese
   final String pinyin;
 
-  Word(
-      {required this.topic, 
-      required this.english,
-      required this.character,
-      required this.pinyin});
+  Word({required this.topic, required this.english, required this.character, required this.pinyin});
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,3 @@
-import 'package:chinese_flashcard/animations/fadein_animation.dart';
-import 'package:chinese_flashcard/components/homme_page/topic_cards.dart';
-import 'package:chinese_flashcard/configs/constants.dart';
-import 'package:chinese_flashcard/configs/extenstions.dart';
-import 'package:chinese_flashcard/data/words.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,34 +8,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final List _topics = [];
-
-  @override
-  void initState() {
-    super.initState();
-    for (var t in words) {
-      if (!_topics.contains(t.topic)) {
-        _topics.add(t.topic);
-      }
-      _topics.sort();
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    final paddingWidth = size.width * 0.04;
     return Scaffold(
-      // backgroundColor: context.theme.scaffoldBackgroundColor,
-      appBar: AppBar(
-          // backgroundColor: context.theme.primaryColor,
-          centerTitle: true,
-          // titleTextStyle: context.theme.appBarTheme.titleTextStyle,
-          toolbarHeight: size.height * 0.15,
+        appBar: AppBar(
+          toolbarHeight: size.height * 0.10,
+          //round the bottom-left and bottom-right corners of Appbar
           shape: const RoundedRectangleBorder(
               borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(32.0),
-                  bottomRight: Radius.circular(32.0))),
+                  bottomLeft: Radius.circular(32.0), bottomRight: Radius.circular(32.0))),
           title: const Text.rich(
               textAlign: TextAlign.center,
               TextSpan(children: [
@@ -49,37 +26,27 @@ class _HomePageState extends State<HomePage> {
                 ),
                 TextSpan(
                   text: "\n中文抽认卡",
-                  style: TextStyle(color: kYellow),
+                  style: TextStyle(color: Colors.yellow),
                 ),
-              ]))),
-      body: Padding(
-        padding: EdgeInsets.only(left: paddingWidth, right: paddingWidth),
-        child: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              backgroundColor: context.theme.scaffoldBackgroundColor,
-              expandedHeight: size.height * 0.40,
-              flexibleSpace: FlexibleSpaceBar(
-                background: Padding(
-                  padding: EdgeInsets.all(size.width * 0.10),
-                  child: FadeInAnimation(
-                      child: Image.asset("assets/images/drangon.png")),
-                ),
-              ),
+              ])),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CustomScrollView(slivers: [
+            const SliverAppBar(
+              title: Text('Image placeHolder'),
             ),
-            //
             SliverGrid(
                 delegate: SliverChildBuilderDelegate(
-                  childCount: _topics.length,
-                  (context, index) => FadeInAnimation(child: TopicCards(topics: _topics[index])),
+                  (context, index) {
+                    return Container(
+                      color: Colors.red,
+                    );
+                  },
                 ),
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    crossAxisSpacing: 6.0,
-                    mainAxisSpacing: 6.0))
-          ],
-        ),
-      ),
-    );
+                    crossAxisCount: 3, crossAxisSpacing: 8.0, mainAxisSpacing: 8.0))
+          ]),
+        ));
   }
 }
